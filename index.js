4b55d1ad1ed13ac85e349af7a1106d33d9215907
Eyman Eghterafi <eghterafieyman@gmail.com>
2021-12-30 17:50:49 -0600

    commit tree c6e05bea84350d752dc2a50983fa42c16f873457

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(driversArr, nameStr){
+    const found = driversArr.filter(name => {
+        return name.toLowerCase() === nameStr.toLowerCase()
+    })
+    return found
+}
+
+function fuzzyMatch(driversArr, nameStr) {
+    const matchedArray = driversArr.filter(name => {
+        return name.startsWith(nameStr)
+    })
+    return matchedArray
+}
+
+function matchName(arrayOfDriverObj, string){
+    const elementMatchesStr = arrayOfDriverObj.filter(person => {
+        // console.log(name.hometown === string)
+        return person.name.toLowerCase() === string.toLowerCase()
+    }) 
+    return elementMatchesStr
+}
